219c7f4fee3a69753edb0afb51569e9fc4ae2b0f
Nan Lu <nan.lu@cern.ch>
2018-01-31 08:43:44 +0100

    add json file

--- crabjob/scrabconfig_data.py
+++ crabjob/scrabconfig_data.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'MIP_DY_wPU_test_crab_v4'
+config.General.requestName = 'MIP_DY_wPU_test_crab_v4'  #change name
 config.General.workArea = 'crab_prod'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -9,12 +9,13 @@ config.General.transferLogs = True
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'HEP17_cfg.py'
 
-config.Data.inputDataset = '/DYToMuMu_M-20_13TeV_pythia8/RunIISummer17DRPremix-92X_upgrade2017_realistic_v10-v2/GEN-SIM-RECO'
+config.Data.inputDataset = '/DYToMuMu_M-20_13TeV_pythia8/RunIISummer17DRPremix-92X_upgrade2017_realistic_v10-v2/GEN-SIM-RECO'#change the name of the dataset
 config.Data.inputDBS = 'global'
+Data.lumiMask = 'dataJSON/Cert_294927-302654_13TeV_PromptReco_Collisions17_JSON.txt'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 10
-config.Data.outLFNDirBase = '/store/group/dpg_hcal/comm_hcal/nlu'
+config.Data.outLFNDirBase = '/store/group/dpg_hcal/comm_hcal/nlu' #change user name
 config.Data.publication = True
-config.Data.outputDatasetTag = 'DY_wPU_test_crab_v4'
+config.Data.outputDatasetTag = 'DY_wPU_test_crab_v4' #change name
 
 config.Site.storageSite = 'T2_CH_CERN'
