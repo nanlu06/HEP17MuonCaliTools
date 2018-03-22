4eac7d1b66afb445584a24d3ca6ea5709b0fa33b
Nan Lu <nan.lu@cern.ch>
2018-03-22 21:40:51 +0100

    add pathlength correction

--- tools/AnalysisTools/src/graph_pileupCorr_perDetID_test.C
+++ tools/AnalysisTools/src/graph_pileupCorr_perDetID_test.C
@@ -92,8 +92,8 @@ float x4[n]={ 1.166e+01,1.813e+01,2.463e+01,3.261e+01 };
         TTree *outtree = new TTree("outtree_pileup_corr", "Info");
         cout <<"create tree"<<endl;
 
-        float energy, energy_errhigh, energy_errlow;
-        energy = -999, energy_errhigh = 999, energy_errlow = 999;
+        float energy, energy_errhigh, energy_errlow, energy_per_layer, energy_per_layer_errhigh, energy_per_layer_errlow, energy_corr, energy_corr_errhigh, energy_corr_errlow;
+        energy = -999, energy_errhigh = 999, energy_errlow = 999, energy_per_layer = -999, energy_per_layer_errlow = 999, energy_per_layer_errhigh = 999, energy_corr = -999, energy_corr_errhigh = 999, energy_corr_errlow = 999;
  
         outtree->Branch("eta", &eta, "eta/I");
         outtree->Branch("phi", &phi, "phi/I");
@@ -101,12 +101,120 @@ float x4[n]={ 1.166e+01,1.813e+01,2.463e+01,3.261e+01 };
         outtree->Branch("energy", &energy, "energy/F");
         outtree->Branch("energy_errhigh", &energy_errhigh, "energy_errhigh/F");
         outtree->Branch("energy_errlow", &energy_errlow, "energy_errlow/F");
-
+        outtree->Branch("energy_per_layer", &energy_per_layer, "energy_per_layer/F");
+        outtree->Branch("energy_per_layer_errhigh", &energy_per_layer_errhigh, "energy_per_layer_errhigh/F");
+        outtree->Branch("energy_per_layer_errlow", &energy_per_layer_errlow, "energy_per_layer_errlow/F");
+        outtree->Branch("energy_corr", &energy_corr, "energy_corr/F"); 
+        outtree->Branch("energy_corr_errhigh", &energy_corr_errhigh, "energy_corr_errhigh/F");
+        outtree->Branch("energy_corr_errlow", &energy_corr_errlow, "energy_corr_errlow/F");
         energy = prod.first;
         energy_errhigh = prod.second;
         energy_errlow = prod.second;
 
-        outtree->Fill();
+        if(eta==16) energy_per_layer = prod.first/3.;
+        else if(eta==17) energy_per_layer = prod.first/5.;
+        if(eta>17){
+           if(depth==2 || depth==4){
+              if(eta==26) energy_per_layer = prod.first/2.;
+              else energy_per_layer = prod.first/3.;
+           }
+           else if(depth==3){
+              if(eta==26) energy_per_layer = prod.first/2.;
+              else if(eta==18) energy_per_layer = prod.first/4.;
+              else energy_per_layer = prod.first/3.;
+           }
+           else if(depth>4){
+              energy_per_layer = prod.first/4.;
+           }
+        }
+        
+        energy_per_layer_errhigh = energy_per_layer*energy_errhigh/energy;
+        energy_per_layer_errlow = energy_per_layer*energy_errlow/energy;
+
+        if(eta==16 && depth==4) energy_corr = prod.first/2.5409214;
+        else if(eta==17){
+             if(depth==2) energy_corr = prod.first/2.9010239;
+             else if(depth==3)  energy_corr = prod.first/2.9010239;
+        }  
+        else if(eta==18){
+             if(depth==2) energy_corr = prod.first/1.2209443;
+             else if(depth==3)  energy_corr = prod.first/1.6279258;
+             else if(depth==4)  energy_corr = prod.first/1.2209443;
+             else if(depth==5)  energy_corr = prod.first/2.4418887;
+        }
+        else if(eta==19){
+             if(depth==1) energy_corr = prod.first/0.9749902;
+             else if(depth==2) energy_corr = prod.first/1.2024880;
+             else if(depth==3)  energy_corr = prod.first/1.2024880;
+             else if(depth==4)  energy_corr = prod.first/1.2024880;
+             else if(depth==5)  energy_corr = prod.first/1.6033173;
+             else if(depth==6)  energy_corr = prod.first/1.6033173;
+        }
+        else if(eta==20){
+             if(depth==1) energy_corr = prod.first/0.9625976;
+             else if(depth==2) energy_corr = prod.first/1.1872037;
+             else if(depth==3)  energy_corr = prod.first/1.1872037;
+             else if(depth==4)  energy_corr = prod.first/1.1872037;
+             else if(depth==5)  energy_corr = prod.first/1.5829383;
+             else if(depth==6)  energy_corr = prod.first/1.5829383;
+        }
+        else if(eta==21){
+             if(depth==1) energy_corr = prod.first/0.9521488;
+             else if(depth==2) energy_corr = prod.first/1.1743168;
+             else if(depth==3)  energy_corr = prod.first/1.1743168;
+             else if(depth==4)  energy_corr = prod.first/1.1743168;
+             else if(depth==5)  energy_corr = prod.first/1.5657558;
+             else if(depth==6)  energy_corr = prod.first/1.565755;
+        }
+        else if(eta==22){
+             if(depth==1) energy_corr = prod.first/0.9429098;
+             else if(depth==2) energy_corr = prod.first/1.1629221;
+             else if(depth==3)  energy_corr = prod.first/1.1629221;
+             else if(depth==4)  energy_corr = prod.first/1.1629221;
+             else if(depth==5)  energy_corr = prod.first/1.5505628;
+             else if(depth==6)  energy_corr = prod.first/1.5505628;
+        }
+        else if(eta==23){
+             if(depth==1) energy_corr = prod.first/0.9345199;
+             else if(depth==2) energy_corr = prod.first/1.1525746;
+             else if(depth==3)  energy_corr = prod.first/1.1525746;
+             else if(depth==4)  energy_corr = prod.first/1.1525746;
+             else if(depth==5)  energy_corr = prod.first/1.5367661;
+             else if(depth==6)  energy_corr = prod.first/1.5367661;
+        }
+        else if(eta==24){
+             if(depth==1) energy_corr = prod.first/0.9269888;
+             else if(depth==2) energy_corr = prod.first/1.1432862;
+             else if(depth==3)  energy_corr = prod.first/1.1432862;
+             else if(depth==4)  energy_corr = prod.first/1.1432862;
+             else if(depth==5)  energy_corr = prod.first/1.5243816;
+             else if(depth==6)  energy_corr = prod.first/1.5243816;
+        }
+        else if(eta==25){
+             if(depth==1) energy_corr = prod.first/0.9203438;
+             else if(depth==2) energy_corr = prod.first/1.1350908;
+             else if(depth==3)  energy_corr = prod.first/1.1350908;
+             else if(depth==4)  energy_corr = prod.first/1.1350908;
+             else if(depth==5)  energy_corr = prod.first/1.5134544;
+             else if(depth==6)  energy_corr = prod.first/1.5134544;
+        }
+        else if(eta==26){
+             if(depth==1) energy_corr = prod.first/0.9146088;
+             else if(depth==2) energy_corr = prod.first/0.7520116;
+             else if(depth==3)  energy_corr = prod.first/0.7520116;
+             else if(depth==4)  energy_corr = prod.first/0.7520116;
+             else if(depth==5)  energy_corr = prod.first/1.1280175;
+             else if(depth==6)  energy_corr = prod.first/1.5040233;
+             else if(depth==7)  energy_corr = prod.first/1.5040233;
+        }
+        energy_corr_errhigh = energy_corr*energy_errhigh/energy; 
+        energy_corr_errlow = energy_corr*energy_errlow/energy;
+
+        if(depth==4 && eta==16) outtree->Fill();
+        else if((depth==2 || depth==3) && eta==17) outtree->Fill();
+        else if((depth>1 && depth<6) && eta==18) outtree->Fill();
+        else if(depth<7 && (eta>18 || eta<26)) outtree->Fill();
+        else if(depth<8 && eta==26) outtree->Fill();
 
         TCanvas *c1 = new TCanvas("c1","pile up correction plot",200,10,700,500);
         gr1->Draw("AP");
@@ -141,6 +249,6 @@ float x4[n]={ 1.166e+01,1.813e+01,2.463e+01,3.261e+01 };
          
         TFile* outputFile=TFile::Open(Form("%s/NoIso_test_pileupCorr_depth%d_iphi%d_ieta%d.root", path.Data(), depth, phi, eta),"recreate");
         outtree->Write();
-        gr1->SetName("gr1");
+        gr1->SetName(Form("gr_ieta%d_idepth%d",eta,depth));
         gr1->Write();
 }
