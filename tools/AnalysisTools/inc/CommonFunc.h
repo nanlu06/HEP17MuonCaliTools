3620fd44d9bb825595a268924d5f8521810edafa
Nan Lu <nan.lu@cern.ch>
2018-05-22 10:51:48 +0200

    style

--- tools/AnalysisTools/inc/CommonFunc.h
+++ tools/AnalysisTools/inc/CommonFunc.h
@@ -4,10 +4,12 @@
 
 namespace CommonFunc{
   void PrintCanvas(TCanvas* c, TString name);
+  std::pair<float,float> LinearCal( TF1 *myfunc4);
   bool IsNaN(double var);
   TStyle* AtlasStyle();
   void SetAtlasStyle();
   bool AddCMS( TCanvas* C );
+  void setTDRStyle();
 };
 
 
